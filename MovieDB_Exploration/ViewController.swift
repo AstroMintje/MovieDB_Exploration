b60f809ccf7932f3c90b5a0acd40618303f3d192
AstroMintje <michael.mintje@binus.ac.id>
2026-08-04 17:09:06 +0700

    Initial Commit

---
+++ MovieDB_Exploration/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MovieDB_Exploration
+//
+//  Created by Michael Mintje on 04/08/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
