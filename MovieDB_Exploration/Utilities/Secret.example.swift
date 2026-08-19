95c17a4ea0712fb7b5c503a940f9433322f1ae57
AstroMintje <michael.mintje@binus.ac.id>
2026-08-19 16:04:53 +0700

    Remove Secret example template

--- MovieDB_Exploration/Utilities/Secret.example.swift
+++
@@ -1,9 +0,0 @@
-//
-//  Secret.example.swift
-//  MovieDB_Exploration
-//
-//  Created by Michael Mintje on 10/08/26.
-//
-enum Secrets {
-    static let tmdbAccessToken = "YOUR_TMDB_ACCESS_TOKEN_HERE"
-}
